bc5b1258a92a903d5185d63248dca5d26c77ddc3
shj <kan02134@gmail.com>
2020-07-29 01:33:46 +0900

    Remove vue-router

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import store from './store';
-import router from './router';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
@@ -10,6 +9,5 @@ Vue.config.productionTip = false;
 new Vue({
   store,
   vuetify,
-  router,
   render: (h) => h(App),
 }).$mount('#app');
